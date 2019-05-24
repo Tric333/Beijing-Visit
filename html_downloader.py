bad0a8e418b57748e5eaf6a78f91d08722a00f21
Tric333 <34879209+Tric333@users.noreply.github.com>
2019-05-24 16:23:19 +0800

    Create html_downloader.py

---
+++ html_downloader.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'zyx'
+import requests
+
+class HtmlDownloader(object):
+    #函数编解码方式应该从web页面获取
+    def download(self, url,decoding='gbk',encoding='utf-8'):
+        if url is None:
+            return None
+        html=requests.get(url)
+        html=html.content.decode(decoding)
+        return html
